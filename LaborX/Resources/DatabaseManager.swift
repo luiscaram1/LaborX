4bb938deacdff9140a452d7e8340ade0e57b4357
Luis Caram <luiscaram@Luiss-MBP.lan>
2020-08-29 18:11:01 -0500

    added safe email replacing characters

--- LaborX/Resources/DatabaseManager.swift
+++ LaborX/Resources/DatabaseManager.swift
@@ -25,7 +25,10 @@ extension DatabaseManager {
     public func userExists(with email: String,
                            completion: @escaping((Bool) -> Void)) {
         
-        database.child(email).observeSingleEvent(of: .value, with: { snapshot in
+        var safeEmail = email.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        
+        database.child(safeEmail).observeSingleEvent(of: .value, with: { snapshot in
             guard snapshot.value as? String  != nil else {
                 completion(false)
                 return
@@ -37,7 +40,7 @@ extension DatabaseManager {
     
     /// Inserts new user to Databse
     public func insertUser(with user: ChatAppUser) {
-        database.child(user.emailAddress).setValue([
+        database.child(user.safeEmail).setValue([
             "first_name": user.firstName,
             "last_name": user.lastName
             //"email_address": user.emailAddress
@@ -49,6 +52,13 @@ extension DatabaseManager {
         let firstName: String
         let lastName: String
         let emailAddress: String
+        
+        var safeEmail: String {
+            var safeEmail = emailAddress.replacingOccurrences(of: ".", with: "-")
+            safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+            return safeEmail
+        }
+        
         //let profilePictureUrl: String
     }
     

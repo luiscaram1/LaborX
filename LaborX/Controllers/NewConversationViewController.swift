c8daa8d143611edb05f1750e176068f1083c4d0d
Luis Caram <luiscaram@icloud.com>
2020-09-10 00:53:27 -0500

    fixed search cell adding a new file named view conversation swift file. copying from customviewcell

--- LaborX/Controllers/NewConversationViewController.swift
+++ LaborX/Controllers/NewConversationViewController.swift
@@ -12,13 +12,13 @@ import JGProgressHUD
 class NewConversationViewController: UIViewController {
     
     
-    public var completion: (([String: String]) -> (Void))?
+    public var completion: ((SearchResult) -> (Void))?
     
     private let spinner = JGProgressHUD(style: .dark)
     
     private var users = [[String: String]]()
     
-    private var results = [[String: String]]()
+    private var results = [SearchResult]()
     
     private var hasFetched = false
     
@@ -31,8 +31,8 @@ class NewConversationViewController: UIViewController {
     private let tableView: UITableView = {
         let table = UITableView()
         table.isHidden = true
-        table.register(UITableViewCell.self,
-                       forCellReuseIdentifier: "cell")
+        table.register(NewConversationCell.self,
+                       forCellReuseIdentifier: NewConversationCell.identifier)
         return table
     }()
     
@@ -82,8 +82,11 @@ extension NewConversationViewController: UITableViewDelegate, UITableViewDataSou
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = results[indexPath.row]["name"]
+        let model = results[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier:NewConversationCell.identifier,
+                                                 for: indexPath) as! NewConversationCell
+        
+        cell.configure(with: model)
         return cell
     }
     
@@ -96,6 +99,10 @@ extension NewConversationViewController: UITableViewDelegate, UITableViewDataSou
         dismiss(animated: true, completion: {  [weak self] in
             self?.completion?(targetUserData)
         })
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 90
     }
 }
 
@@ -139,19 +146,33 @@ extension NewConversationViewController: UISearchBarDelegate {
     
     func filterUsers(with term: String) {
         //update the UI: either show results or show no results label
-        guard hasFetched else {
+        guard let currentUserEmail = UserDefaults.standard.value(forKey: "email") as? String, hasFetched else {
             return
         }
         
+        let safeEmail = DatabaseManager.safeEmail(emailAddress: currentUserEmail)
+        
         self.spinner.dismiss()
         
-        let results: [[String: String]] = self.users.filter({
+        let results: [SearchResult] = self.users.filter({
+            guard let email = $0["email"], email != safeEmail else {
+                    return false
+            }
+            
             guard let name = $0["name"]?.lowercased() else {
                 return false
             }
             
             return name.hasPrefix(term.lowercased())
-        })
+            }).compactMap({
+                
+                guard let email = $0["email"],
+                    let name = $0["name"]?.lowercased() else {
+                    return nil
+                }
+                
+                return SearchResult(name: name, email: email)
+            })
         
         self.results = results
         
@@ -170,4 +191,9 @@ extension NewConversationViewController: UISearchBarDelegate {
             self.tableView.reloadData()
         }
     }
+}
+
+struct SearchResult {
+    let name: String
+    let email: String
 }
